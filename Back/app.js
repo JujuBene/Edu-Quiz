ec3a04399dc3203ed8f37d50a77c45c748448679
Pedro <pedroleonardo2001@gmail.com>
2025-06-05 17:53:47 -0300

    Feature: Metodo get funcionando, pegando as info do banco de dados

--- Back/app.js
+++ Back/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose")
 const app = express();
 const port = 5500
 const Category = require('./Models/Category')
+app.use(express.json());
 
 
 
@@ -49,8 +50,22 @@ const categoryQuestions  = {
 
 
 
-app.get('/',(req,res)=>{
-    res.send("Allan é um babaca!!")
+app.get('/category/:name',async(req,res)=>{
+     try {
+    const { name } = req.params;
+    console.log(name);
+
+    const category = await Category.findOne({name});
+
+    if (!category) {
+      return res.status(404).send({ message: "Categoria não encontrada" });
+    }
+
+    res.send(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Erro ao buscar categoria" });
+  }
 })
 
 app.listen(port,async()=>{
@@ -60,23 +75,28 @@ app.listen(port,async()=>{
 })
 
 
-app.post('/insert',(req,res)=>{
-
-    bd = seedCategories()
-    // bd = "foi"
-    res.status(200).send(bd)
+app.post('/insert',async(req,res)=>{
+     try {
+        const result = await seedCategories();
+        res.status(200).json({ message: 'Categorias inseridas com sucesso', result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro ao inserir categorias' });
+    }
 })
 
 
 async function seedCategories(){
+    await Category.insertMany(categories);
     const categories = Object.entries(categoryQuestions).map(([name,questions])=>({
         name,
         questions
     }))
+    await Category.deleteMany()
 
-    await Category.insertMany(categories);
     console.log(categoryQuestions)
     console.log('Categoria inseriada com sucesso')
+    return categoryQuestions
 }
 
 
